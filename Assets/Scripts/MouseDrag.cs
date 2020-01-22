412f40d318b35341814ee5cf4f15927c114b594c
Milos Kojic <m.kojic@live.com>
2020-01-22 19:36:08 +0000

    Simple mouse drag

---
+++ Assets/Scripts/MouseDrag.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseDrag : MonoBehaviour
+{
+    bool isMouseDown;
+    SpringJoint2D springJoint;
+
+    private void Awake()
+    {
+        springJoint = GetComponent<SpringJoint2D>();
+    }
+    void OnMouseDown()
+    {
+        isMouseDown = true;
+        springJoint.enabled = false;
+    }
+
+    private void OnMouseUp()
+    {
+        isMouseDown = false;
+        springJoint.enabled = true;
+    }
+
+    void Update()
+    {
+        if (isMouseDown)
+        {
+            Vector3 mouseWorldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = new Vector3(mouseWorldPoint.x, mouseWorldPoint.y, 0);
+        }
+
+
+    }
+}
