d85ee6daff8a29dd0194b701026d057dbfc093d0
Milos Kojic <m.kojic@live.com>
2020-01-22 20:14:45 +0000

    Trail renderer

--- Assets/Scripts/MouseDrag.cs
+++ Assets/Scripts/MouseDrag.cs
@@ -6,21 +6,25 @@ public class MouseDrag : MonoBehaviour
 {
     bool isMouseDown;
     SpringJoint2D springJoint;
+    TrailRenderer trailRenderer;
 
     private void Awake()
     {
         springJoint = GetComponent<SpringJoint2D>();
+        trailRenderer = GetComponent<TrailRenderer>();
     }
     void OnMouseDown()
     {
         isMouseDown = true;
         springJoint.enabled = false;
+        trailRenderer.enabled = false;
     }
 
     private void OnMouseUp()
     {
         isMouseDown = false;
         springJoint.enabled = true;
+        trailRenderer.enabled = true;
     }
 
     void Update()
