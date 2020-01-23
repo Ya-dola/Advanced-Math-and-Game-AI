0f364bebe3fbace938e94ef797b2ae4e8336cd4a
Milos Kojic <m.kojic@live.com>
2020-01-23 09:55:01 +0000

    Reset weapon position

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class LevelManager : MonoBehaviour
 {
     public static LevelManager Instance;
+    public float ResetSpeed = 1f;
     private void Awake()
     {
         Instance = this;
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    Rigidbody2D rigidbody2d;
+    MouseDrag mouseDrag;
+    SpringJoint2D spring;
+    Vector2 initialPosition;
+    private void Awake()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        mouseDrag = GetComponent<MouseDrag>();
+        spring = GetComponent<SpringJoint2D>();
+        initialPosition = transform.position;
+    }
+
+    void Update()
+    {
+        if(!spring.enabled && !mouseDrag.isMouseDown && rigidbody2d.velocity.magnitude < LevelManager.Instance.ResetSpeed)
+        {
+            ResetWeapon();
+        }
+    }
+
+    private void ResetWeapon()
+    {
+        transform.position = initialPosition;
+        spring.enabled = true;
+    }
+}
