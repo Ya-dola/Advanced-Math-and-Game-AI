f417e5db5c5dc499e12966c2cb0d29f908cf1d84
Milos Kojic <m.kojic@live.com>
2020-01-23 18:09:51 +0000

    Damage power up

---
+++ Assets/Scripts/PowerUps/DamagePowerUp.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DamagePowerUp : MonoBehaviour, IPowerUp
+{
+    public Player player;
+    public int Damage = 20;
+    public int ActiveFor = 10;
+    public Sprite regularSprite;
+    public Sprite powerUpSprite;
+
+    public void Activate()
+    {
+        GetComponent<Button>().interactable = false;
+        player.weaponData.Damage += Damage;
+        player.Weapon.GetComponent<SpriteRenderer>().sprite = powerUpSprite;
+        StartCoroutine(HoldActive());
+    }
+    public void Deactivate()
+    {
+        player.weaponData.Damage -= Damage;
+        player.Weapon.GetComponent<SpriteRenderer>().sprite = regularSprite;
+    }
+
+    public IEnumerator HoldActive()
+    {
+        yield return new WaitForSeconds(ActiveFor);
+        Deactivate();
+    }
+}
