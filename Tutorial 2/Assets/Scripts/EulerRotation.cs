053f68ebe58b51b7b56bcb544e4b482a3d19a7af
Milos Kojic <m.kojic@live.com>
2020-01-29 16:05:36 +0000

    Euler angles and quaternions rotations

---
+++ Tutorial 2/Assets/Scripts/EulerRotation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EulerRotation : MonoBehaviour
+{
+    //To create the Gimbal lock effect set:
+    //1) CurrentRotation X to 90 
+    //2) Angles to Rotate Z to 90 
+    //CurrentRotation = new Vector3 (90f, 0f, 0f);
+    //AnglesToRotate = new Vector3 (0f, 0f, 90f);
+
+    public Vector3 CurrentRotation;
+    public Vector3 AnglesToRotate;
+
+    void Start()
+    {
+        transform.eulerAngles = CurrentRotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CurrentRotation += AnglesToRotate * Time.deltaTime;
+        transform.eulerAngles = CurrentRotation;
+    }
+}
---
+++ Tutorial 2/Assets/Scripts/QuaternionRotation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuaternionRotation : MonoBehaviour
+{
+    public Vector3 CurrentRotation;
+    public Vector3 AnglesToRotate;
+
+    void Start()
+    {
+        Quaternion yRotation = Quaternion.AngleAxis(CurrentRotation.y, Vector3.up);
+        Quaternion xRotation = Quaternion.AngleAxis(CurrentRotation.x, Vector3.right);
+        Quaternion zRotation = Quaternion.AngleAxis(CurrentRotation.z, Vector3.forward);
+        transform.rotation = yRotation * xRotation * zRotation;
+    }
+
+    void Update()
+    {
+        Quaternion yRotation = Quaternion.AngleAxis(AnglesToRotate.y * Time.deltaTime, Vector3.up);
+        Quaternion xRotation = Quaternion.AngleAxis(AnglesToRotate.x * Time.deltaTime, Vector3.right);
+        Quaternion zRotation = Quaternion.AngleAxis(AnglesToRotate.z * Time.deltaTime, Vector3.forward);
+        transform.rotation = yRotation * xRotation * zRotation * transform.rotation;
+
+        CurrentRotation += AnglesToRotate * Time.deltaTime;
+
+    }
+
+}
