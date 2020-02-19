ae49787f45b82fb869889a71951c87963bc5c605
Milos Kojic <m.kojic@live.com>
2020-02-19 22:53:17 +0000

    asymmetry

---
+++ Tutorial 5/Assets/Scripts/Asymmetry.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asymmetry : MonoBehaviour
+{
+    public BarChart CurrentRollChart;
+    public BarChart AsymmetryChart;
+    public RandomNumberGenerator RandomNumberGenerator;
+
+    List<int> allMax;
+    // Start is called before the first frame update
+    void Start()
+    {
+        allMax = new List<int>();
+    }
+
+    public void Generate()
+    {
+        //Get values of the current roll
+        var maxValues = RandomNumberGenerator.GenerateMax(RandomNumberGenerator.NumberOfRolls, RandomNumberGenerator.NumberOfFaces);
+        var maxGroupedValues = RandomNumberGenerator.GroupAllValues(maxValues);
+        var currentAverage = (int)Math.Round(RandomNumberGenerator.GetAverage(maxGroupedValues), MidpointRounding.AwayFromZero);
+        allMax.Add(currentAverage);
+
+        //Normalize average values for visualization
+        //var normalizedValues = new List<int>();
+        //var averageCount = allAverages.Count;
+        //foreach (var item in allAverages)
+        //{
+        //    normalizedValues.Add(item / averageCount * RandomNumberGenerator.NumberOfFaces);
+        //}
+
+        var totalGroupedValues = RandomNumberGenerator.GroupAllValues(allMax);
+
+        //Draw charts
+        CurrentRollChart.DrawChart(maxGroupedValues);
+        AsymmetryChart.DrawChart(totalGroupedValues);
+
+    }
+}
--- Tutorial 5/Assets/Scripts/RandomNumberGenerator.cs
+++ Tutorial 5/Assets/Scripts/RandomNumberGenerator.cs
@@ -33,6 +33,25 @@ public class RandomNumberGenerator : MonoBehaviour
         return result;
     }
 
+    public List<int> GenerateMax(int numberOfRolls, int numberOfFaces)
+    {
+        var result = new List<int>();
+        for (int j = 0; j < numberOfRolls; j++)
+        {
+            var currentRollValues = new List<int>();
+            for (int i = 0; i < numberOfRolls; i++)
+            {
+                var randomNumber = Random.Range(1, numberOfFaces + 1);
+                currentRollValues.Add(randomNumber);
+            }
+            var max = GetMax(currentRollValues);
+            result.Add(max);
+        }
+        
+
+        return result;
+    }
+
     public void PrintAllValues(List<int> list)
     {
         var allValues = "";
@@ -71,6 +90,19 @@ public class RandomNumberGenerator : MonoBehaviour
         }
 
         return result / NumberOfFaces;
+    }
+
+    public int GetMax(List<int> list)
+    {
+        var max = 0;
+        foreach (var item in list)
+        {
+            if(item > max)
+            {
+                max = item;
+            }
+        }
+        return max;
     }
 
 }
