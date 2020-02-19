e42f46c40566c37afcf9f5600189ab283e22b192
Milos Kojic <m.kojic@live.com>
2020-02-19 16:17:19 +0000

    Chart

---
+++ Tutorial 5/Assets/Scripts/BarChart.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class BarChart : MonoBehaviour
+{
+    public int XSpacing;
+    public int YSpacing;
+    public GameObject ChartBar;
+    public TextMeshPro Text;
+    public RandomNumberGenerator randomNumberGenerator;
+
+    private Transform HorizontalTexts;
+    private Transform VerticalTexts;
+    private Transform Bars;
+    // Start is called before the first frame update
+    void Start()
+    {
+        HorizontalTexts = transform.GetChild(0);
+        VerticalTexts = transform.GetChild(1);
+        Bars = transform.GetChild(2);
+        CreateChart();
+    }
+
+    public void RebuildChart()
+    {
+        DestroyChildrenObjects(HorizontalTexts);
+        DestroyChildrenObjects(VerticalTexts);
+        DestroyChildrenObjects(Bars);
+        
+        CreateChart();
+    }
+
+    public void CreateChart()
+    {
+        var values = randomNumberGenerator.Generate(randomNumberGenerator.NumberOfRolls, randomNumberGenerator.NumberOfFaces);
+        var groupedValues = randomNumberGenerator.GroupAllValues(values);
+
+        for (int i = 1; i <= randomNumberGenerator.NumberOfFaces; i++)
+        {
+            if (groupedValues.ContainsKey(i))
+            {
+                var yScale = groupedValues[i];
+                var bar = Instantiate(ChartBar, new Vector3(i + (i * XSpacing), 0, 0), Quaternion.identity, Bars);
+                bar.transform.localScale = new Vector3(1, yScale + (yScale * YSpacing), 1);
+            }
+
+            //horizontal text
+            var xText = Instantiate(Text.gameObject, new Vector3(i + (i * XSpacing), -1, 0), Quaternion.identity, HorizontalTexts);
+            xText.GetComponent<TextMeshPro>().text = i.ToString();
+
+            //vertical text
+            var yText = Instantiate(Text.gameObject, new Vector3(-1, i + (i * YSpacing), 0), Quaternion.identity, VerticalTexts);
+            yText.GetComponent<TextMeshPro>().text = i.ToString();
+        }
+    }
+
+    private void DestroyChildrenObjects(Transform parent)
+    {
+        foreach (Transform item in parent)
+        {
+            Destroy(item.gameObject);
+        }
+    }
+}
---
+++ Tutorial 5/Assets/Scripts/RandomNumberGenerator.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class RandomNumberGenerator : MonoBehaviour
+{
+    public int NumberOfRolls;
+    public int NumberOfFaces;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //var randomList = Generate(NumberOfRolls, NumberOfFaces);
+        //PrintAllValues(randomList);
+        //GroupAllValues(randomList);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public List<int> Generate(int numberOfRolls, int numberOfFaces)
+    {
+        var result = new List<int>();
+        for (int i = 0; i < numberOfRolls; i++)
+        {
+            var randomNumber = Random.Range(1, numberOfFaces);
+            result.Add(randomNumber);
+        }
+
+        return result;
+    }
+
+    private void PrintAllValues(List<int> list)
+    {
+        var allValues = "";
+        foreach (var item in list)
+        {
+            allValues += item + " ";
+        }
+
+        Debug.Log(allValues);
+    }
+
+    public Dictionary<int, int> GroupAllValues(List<int> list)
+    {
+        var result = new Dictionary<int,int>();
+        result = list.GroupBy(v => v).ToDictionary(v => v.Key, v => v.Count());
+
+        foreach (var item in result)
+        {
+            Debug.Log(item.Key + ": " + item.Value);
+        }
+
+        return result;
+
+    }
+
+}
