f46f53885d1c8b07f124aab19f1eb721faa02cd5
Milos Kojic <m.kojic@live.com>
2020-02-20 21:30:37 +0000

    Simple terrain generator scenes

--- Tutorial 5/Assets/Scripts/SimpleTerrainGenerator.cs
+++ Tutorial 5/Assets/Scripts/SimpleTerrainGenerator.cs
@@ -3,26 +3,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TerrainGenerator : MonoBehaviour
+public class SimpleTerrainGenerator : MonoBehaviour
 {
     [Header("Terrain generation")]
     public int Width;
     public int Depth;
     public Gradient gradient;
-    public int Seed;
-    [Range(1,100)]
-    public int Octaves;
-    [Range(1, 100)]
-    public float NoiseScale;
-    [Range(0, 1)]
-    public float Persistance;
-    [Range(1, 100)]
-    public float Lacunarity;
-    [Range(1, 100)]
-    public float HeightMultiplier;
-    [Range(0, 1)]
-    public float HeightTreshhold;
-    public Vector2 Offset;
 
     [Header("Vertex visualization")]
     public GameObject VertexObject;
@@ -37,7 +23,7 @@ public class TerrainGenerator : MonoBehaviour
     private float minHeight;
     private float maxHeight;
     // Start is called before the first frame update
-    void Update()
+    void Start()
     {
         mesh = new Mesh();
         mesh.name = "Procedural Terrain";
@@ -74,19 +60,22 @@ public class TerrainGenerator : MonoBehaviour
     {
         //Vertices
         vertices = new Vector3[(Width + 1) * (Depth + 1)];
-        var noiseArray = PerlinNoise();
 
         int i = 0;
         for (int z = 0; z <= Depth; z++)
         {
             for (int x = 0; x <= Width; x++)
             {
-                var currentHeight = noiseArray[i];
-                if (currentHeight > HeightTreshhold)
+                float y = Mathf.PerlinNoise(x * 0.4f, z * 0.4f) * 3f;
+                vertices[i] = new Vector3(x, y, z);
+                if (y > maxHeight)
                 {
-                    currentHeight *= HeightMultiplier;
+                    maxHeight = y;
                 }
-                vertices[i] = new Vector3(x, currentHeight, z);
+                if (y < minHeight)
+                {
+                    minHeight = y;
+                }
                 i++;
             }
         }
@@ -132,77 +121,10 @@ public class TerrainGenerator : MonoBehaviour
         {
             for (int x = 0; x <= Width; x++)
             {
-                float height = Mathf.InverseLerp(minHeight * HeightMultiplier, maxHeight * HeightMultiplier, vertices[i].y);
+                float height = Mathf.InverseLerp(minHeight, maxHeight, vertices[i].y);
                 colors[i] = gradient.Evaluate(height);
                 i++;
             }
         }
-    }
-
-    float[] PerlinNoise()
-    {
-        float[] noiseArray = new float[(Width + 1) *  (Depth + 1)];
-
-        System.Random prng = new System.Random(Seed);
-        Vector2[] octaveOffsets = new Vector2[Octaves];
-        for (int i = 0; i < Octaves; i++)
-        {
-            float offsetX = prng.Next(-100000, 100000) + Offset.x;
-            float offsetY = prng.Next(-100000, 100000) + Offset.y;
-            octaveOffsets[i] = new Vector2(offsetX, offsetY);
-        }
-
-        float halfWidth = Width / 2f;
-        float halfDepth = Depth / 2f;
-
-        //Apply lacunarity and persistence
-        int n = 0;
-        for (int z = 0; z <= Depth; z++)
-        {
-            for (int x = 0; x <= Width; x++)
-            {
-
-                float amplitude = 1;
-                float frequency = 1;
-                float noiseHeight = 0;
-
-                //Use multiple frequencies (octaves)
-                for (int i = 0; i < Octaves; i++)
-                {
-                    float sampleX = (x - halfWidth) / NoiseScale * frequency + octaveOffsets[i].x;
-                    float sampleY = (z - halfDepth) / NoiseScale * frequency + octaveOffsets[i].y;
-
-                    float perlinValue = Mathf.PerlinNoise(sampleX, sampleY) * 2 - 1;
-                    noiseHeight += perlinValue * amplitude;
-
-                    amplitude *= Persistance;
-                    frequency *= Lacunarity;
-                }
-
-                if (noiseHeight > maxHeight)
-                {
-                    maxHeight = noiseHeight;
-                }
-                else if (noiseHeight < minHeight)
-                {
-                    minHeight = noiseHeight;
-                }
-                noiseArray[n] = noiseHeight;
-                n++;
-            }
-        }
-
-        //Normalize height
-        int k = 0;
-        for (int z = 0; z < Depth; z++)
-        {
-            for (int x = 0; x < Width; x++)
-            {
-                noiseArray[k] = Mathf.InverseLerp(minHeight, maxHeight, noiseArray[k]);
-                k++;
-            }
-        }
-
-        return noiseArray;
     }
 }
